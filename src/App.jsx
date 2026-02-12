18feba16cab21d48d0f7f4c9f9e9b1e3cfa1fde2
GowdaHemannth <hemanthagalahalli@gmail.com>
2026-02-12 22:18:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+
+  // Here Jsut SA Sample For How do i Use the ENV FILES
+  // IN Majorityb of the Files it will be Defined by process thing so 
+  //  Here Below thing will be Used in Most of the Cases But Here we will be Usingthe VITE Project
+ // Since its An Vite App we will Be using the Differnt Things Here
+  // console.log(process.env.REACT_APP_APPWRITE_URL);
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  
+  return (
+    <>
+    <h1>Lets DO Some Real-World-Projects</h1>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+// Here we are Building these Folder Just to Configure all the Api calss You Know why right 
+// Instead of console.log(import.meta.env.VITE_APPWRITE_URL); these in App Everytime Here i will Call these 
+// in APP,JSX INSTAED CALL EVRYTHING AND sTORE IT IN THE vARIBLE hOW DOES THAT sOUND gOOD RIGHT yA wE CAN dO THESE 
+
+const  config={
+   appWriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+   appWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+   appWriteDataBaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+   appWriteCollectionid:String(VITE_APPWRITE_COLLECTION_ID),
+   appWriteBucketid:String(VITE_APPWRITE_BUCKET_ID)
+
+}
+
+export default config
