f74903e017d95eab3140350532ac7d99f9fcaf9c
GowdaHemannth <hemanthagalahalli@gmail.com>
2026-02-23 19:42:10 +0530

    The New FileAdded

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   //  Here Below thing will be Used in Most of the Cases But Here we will be Usingthe VITE Project
  // Since its An Vite App we will Be using the Differnt Things Here
   // console.log(process.env.REACT_APP_APPWRITE_URL);
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
   
   
   return (
